99bf3d5f85c639de759a23b1f23a6a1127ee0656
Ali Khajeh-Hosseini <ali@rightscale.com>
2014-12-23 17:42:35 +0330

    COB-871 Update AUI extension

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,6 +4,16 @@ chrome.runtime.sendMessage({gimme: 'gimme'}, function(req) {
   setInterval(function() {
     var repoPath = location.pathname.split('/').slice(1,3).join('/');
     var repo = options.repos[repoPath];
+    if (!repo) {
+      repo = (function() {
+        for (var name in options.repos) {
+          var repoRegexp = new RegExp('^' + name.replace('*', '.+') + '$', 'i');
+          if (repoRegexp.test(repoPath)) {
+            return options.repos[name];
+          }
+        }
+      })();
+    }
     if (repo && location.pathname.match(/\/pull\/\d/i)) {
       attemptButtonInsertion(repo);
     }
@@ -12,25 +22,48 @@ chrome.runtime.sendMessage({gimme: 'gimme'}, function(req) {
 
 
 function attemptButtonInsertion(repo) {
-  var parent = document.querySelector('.branch-status');
-  var successfulCommits = document.querySelectorAll('.status-success + code>.commit-id');
-  if (successfulCommits && parent) {
-    if (!parent.querySelector('.uiPreviewer')) {
-      var hash = successfulCommits[successfulCommits.length - 1].href.match(/[a-z\d]{40}/i)[0];
-      var link = document.createElement('a');
-      var href = repo.mainButton.urlPattern.replace(/\${gitSha}/, hash);
+  var link, href;
+  if (repo.branch && !document.querySelector('#partial-discussion-header .uiPreviewer')) {
+    var branch = document.querySelector('.current-branch:last-of-type').innerText.trim();
+    var matches = (new RegExp(repo.branch.pattern, 'gi')).exec(branch);
+    if (matches) {
+      link = document.createElement('a');
+      href = repo.branch.urlPattern.replace(/\$(\d)/, function(a, nstr) {
+        var index = parseInt(nstr, 10);
+        return matches[index];
+      });
       link.href = href;
-      link.className = 'minibutton uiPreviewer';
-      link.innerHTML = '<span class="octicon octicon-' + repo.mainButton.icon + '"></span> ' + repo.mainButton.buttonText;
-      link.style.float = 'right';
-      link.style.marginTop = '-0.4em';
-      link.style.marginRight = '0.4em';
+      link.className = 'uiPreviewer';
+      link.innerHTML = '<span class="octicon octicon-' + repo.branch.icon + '"></span> ' + repo.branch.buttonText;
       link.target = '_blank';
-      parent.insertBefore(link, parent.firstChild);
+      document.querySelector('.current-branch:last-of-type').parentElement.appendChild(link);
+    }
+
+  }
+  var parent = document.querySelector('.branch-status');
+  var successfulCommits = Array.prototype.map.call(document.querySelectorAll('.commit-build-statuses  .status-success') || [], function(el) {
+    return el.parentElement.parentElement.querySelector('code>.commit-id')
+  }).filter(function(el) {return el;});
+  if (successfulCommits.length > 0 && parent) {
+    if (!parent.querySelector('.uiPreviewer')) {
+      if (repo.mainButton) {
+        var hash = successfulCommits[successfulCommits.length - 1].href.match(/[a-z\d]{40}/i)[0];
+        link = document.createElement('a');
+        href = repo.mainButton.urlPattern.replace(/\${gitSha}/, hash);
+        link.href = href;
+        link.className = 'minibutton uiPreviewer';
+        link.innerHTML = '<span class="octicon octicon-' + repo.mainButton.icon + '"></span> ' + repo.mainButton.buttonText;
+        link.style.float = 'right';
+        link.style.marginTop = '-0.4em';
+        link.style.marginRight = '0.2em';
+        link.style.marginLeft = '0.4em';
+        link.target = '_blank';
+        parent.insertBefore(link, parent.firstChild);
+      }
 
       if (repo.secondary) {
         secondary = document.createElement('a');
-        var href = repo.secondary.urlPattern.replace(/\${gitSha}/, hash);
+        href = repo.secondary.urlPattern.replace(/\${gitSha}/, hash);
         secondary.href = href;
         secondary.setAttribute('aria-label', repo.secondary.buttonText);
         secondary.className = 'minibutton tooltipped tooltipped-s uiPreviewer';
@@ -38,8 +71,12 @@ function attemptButtonInsertion(repo) {
         secondary.style.float = 'right';
         secondary.style.marginTop = '-0.4em';
         secondary.style.marginRight = '0.4em';
+        secondary.style.marginLeft = '0.2em';
         secondary.target = '_blank';
         parent.insertBefore(secondary, link);
+      }
+      if (document.querySelector('.build-status-description > span:last-of-type')) {
+        document.querySelector('.build-status-description > span:last-of-type').style.width = '60%';
       }
     }
 
--- src/options/options.js
+++ src/options/options.js
@@ -1,6 +1,8 @@
 var prefix = 'uiPreviewerOptions';
 var form = document.getElementById('options_form');
 
+var currentIconField;
+
 var options;
 
 function createUI() {
@@ -13,39 +15,107 @@ function generateRow(name, repo) {
   var div = document.createElement('div');
   div.className = 'repo';
   var source = '';
-  source += '<label><b>Repo:</b> <input type="text" class="repo-name" value="' + name + '" /></label>';
-  source += '<h3>Primary button</h3>';
-  source += '<div class="primary-button">';
-  source += generateButton(repo.mainButton);
-  source += '</div>';
-  source += '<div class="secondary-button">';
-  source += '<h3>Secondary button</h3>';
-  source += '<label><input type="checkbox" ' + (repo.secondary ? 'checked' : '') + '/>Enabled</label>';
-  source += generateButton(repo.secondary || {
-    urlPattern: '',
-    buttonText: '',
-    icon: ''
-  });
-  source += '</div>';
+  source += '<label><b>GitHub Repo:</b> <input type="text" class="repo-name" value="' + name + '" /></label>';
+  source += '<p>Where will these buttons be active on? This can be a wildcard pattern, i.e. <code>angular/*</code></p>';
   div.innerHTML = source;
+
+  div.appendChild(generateButton(repo.mainButton, 'Primary Status Button', 'The primary status button appears as a large button in the \'Merge\' area of the PR and next to any green status circles (as an icon only).', 'primary-button', urlPattern));
+
+  div.appendChild(generateButton(repo.secondary, 'Secondary Status Button', 'The secondary status button appears only as a large button in the \'Merge\' area of the PR.', 'secondary-button', urlPattern));
+
+  div.appendChild(generateButton(repo.branch, 'Branch Button', 'The branch button is used for the common use case where the branch contains information you can use. The button will apear next to the branch name.', 'branch-button', function(div, button) {
+    var out = '';
+    out += '<p><label>Pattern: <input class="general-pattern" type="text" value="'+ button.pattern + '" /></label></p>';
+    out += '<p><label>Url: <input class="pattern" type="text" value="'+ button.urlPattern + '" /></label></p>';
+    out += '<p>The <b>Pattern</b> is a RegExp and will be matched against the branch name. Matched groups 0-9 can be used in the URL to get parts of the branch. To get the whole branch name, simply use <code>.*</code for the pattern and <code>$0</code> in the URL.</p>'
+    return out;
+  }));
   var a = document.createElement('a');
   a.innerText = 'Remove';
   a.addEventListener('click', function(e) {
-    form.removeChild(div);
+    if (confirm('Are you sure you want to delete this repo?')) {
+      form.removeChild(div);
+    }
     e.preventDefault();
   });
   a.href = '#';
+  a.className = 'remove'
   div.appendChild(a);
   return div;
 };
 
-function generateButton(button) {
+function octiconHelp() {
+  var icons = ["alert","alignment-align","alignment-aligned-to","alignment-unalign","arrow-down","arrow-left","arrow-right","arrow-small-down","arrow-small-left","arrow-small-right","arrow-small-up","arrow-up","beer","book","bookmark","briefcase","broadcast","browser","bug","calendar","check","checklist","chevron-down",
+  "chevron-left","chevron-right","chevron-up","circle-slash","circuit-board","clippy","clock","cloud-download","cloud-upload","code","color-mode","comment","comment-discussion","credit-card","dash","dashboard","database","device-camera","device-camera-video","device-desktop","device-mobile","diff","diff-added","diff-ignored","diff-modified",
+  "diff-removed","diff-renamed","ellipsis","eye","file-binary","file-code","file-directory","file-media","file-pdf","file-submodule","file-symlink-directory","file-symlink-file","file-text","file-zip","flame","fold","gear","gift","gist","gist-secret","git-branch","git-commit","git-compare","git-merge","git-pull-request",
+  "globe","graph","heart","history","home","horizontal-rule","hourglass","hubot","inbox","info","issue-closed","issue-opened","issue-reopened","jersey","jump-down","jump-left","jump-right","jump-up","key","keyboard","law","light-bulb","link","link-external","list-ordered",
+  "list-unordered","location","lock","mail","mail-read","mail-reply","mark-github","markdown","megaphone","mention","microscope","milestone","mirror","mortar-board","move-down","move-left","move-right","move-up","mute","no-newline","octoface","organization","package","paintcan",
+  "pencil","person","pin","playback-fast-forward","playback-pause","playback-play","playback-rewind","plug","plus","podium","primitive-dot","primitive-square","pulse","puzzle","question","quote","radio-tower","repo","repo-clone","repo-force-push","repo-forked","repo-pull","repo-push","rocket","rss",
+  "ruby","screen-full","screen-normal","search","server","settings","sign-in","sign-out","split","squirrel","star","steps","stop","sync","tag","telescope","terminal","three-bars","tools","trashcan","triangle-down","triangle-left","triangle-right","triangle-up","unfold",
+  "unmute","versions","x","zap"];
+
+  var div = document.createElement('div');
+  div.className = 'popup';
+  div.id = 'icon-help-popup';
+  icons.forEach(function(icon) {
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'icon';
+    link.innerHTML = '<span class="octicon octicon-' + icon + '"></span>';
+    link.addEventListener('click', function() {
+      currentIconField.value = icon;
+      div.style.display = 'none';
+    });
+    div.appendChild(link);
+  });
+
+  return div;
+}
+
+function urlPattern(div, button) {
+  return '<p><label>Url Pattern: <input type="text" class="pattern" value="' + button.urlPattern + '" /></label></p>' +
+  '<p>You can use <code>${gitSha}</code> where you want the sha of the commit to appear.</p>';
+}
+
+function generateButton(button, title, help, className, cb) {
+  var div = document.createElement('div');
+  div.classList.add('button-section');
+  div.classList.add(className);
   var out = '';
-  out += '<p><label>Url Pattern: <input type="text" class="pattern" value="' + button.urlPattern + '" /></label></p>';
-  out += '<p>You can use <code>${gitSha}</code> where you want the sha of the commit to appear.</p>';
+  out += '<h3>' + title + '</h3>';
+  out += '<p>' + help + '</p>';
+  out += '<label style="color: black"><input type="checkbox" ' + (button ? 'checked' : '') + '/>Enabled</label>';
+  if (!button) {
+    button = {
+      urlPattern: '',
+      buttonText: '',
+      icon: '',
+      pattern: '.*'
+    }
+    div.classList.add('disabled');
+  }
+  out += cb(div, button, className);
   out += '<p><label>Label: <input type="text" class="label" value="' + button.buttonText + '" /></label></p>';
   out += '<p><label>Icon: <input type="text" class="icon" value="' + button.icon + '" /></label></p>';
-  return out;
+  div.innerHTML = out;
+  div.querySelector('input[type=checkbox]').addEventListener('change', function() {
+    if (div.querySelector('input[type=checkbox]').checked) {
+      div.classList.remove('disabled');
+    } else {
+      div.classList.add('disabled');
+    }
+  });
+  var iconLink = document.createElement('a');
+  iconLink.innerText = 'Select';
+  iconLink.addEventListener('click', function(e) {
+    document.getElementById('icon-help-popup').style.display = 'block';
+    currentIconField = div.querySelector('.icon');
+    e.preventDefault();
+  });
+  iconLink.href = '#';
+  var selection = div.querySelectorAll('p');
+  selection[selection.length - 1].appendChild(iconLink);
+  return div;
 }
 
 function save() {
@@ -55,9 +125,14 @@ function save() {
     var repo = repos[i];
     var repoName = repo.querySelector('.repo-name').value;
     var data = {};
-    data.mainButton = getButton(repo.querySelector('.primary-button'));
+    if (repo.querySelector('.primary-button input[type=checkbox]').checked) {
+      data.mainButton = getButton(repo.querySelector('.primary-button'));
+    }
     if (repo.querySelector('.secondary-button input[type=checkbox]').checked) {
       data.secondary = getButton(repo.querySelector('.secondary-button'));
+    }
+    if (repo.querySelector('.branch-button input[type=checkbox]').checked) {
+      data.branch = getButton(repo.querySelector('.branch-button'));
     }
     settings[repoName] = data;
   }
@@ -65,11 +140,15 @@ function save() {
 }
 
 function getButton(button) {
-  return {
+  var obj = {
     urlPattern: button.querySelector('.pattern').value,
     buttonText: button.querySelector('.label').value,
     icon: button.querySelector('.icon').value
   };
+  if (button.querySelector('.general-pattern')) {
+    obj.pattern = button.querySelector('.general-pattern').value
+  }
+  return obj;
 }
 
 chrome.runtime.sendMessage({gimme: 'gimme'}, function(req) {
@@ -85,3 +164,4 @@ document.getElementById('add').addEventListener('click', function() {
     icon: ''
   }}));
 });
+document.body.appendChild(octiconHelp());
