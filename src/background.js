5e420fe8d3996a15c8eb298363dc0a10ff1af304
Jakub Hampl <honitom@seznam.cz>
2016-01-29 17:12:42 +0000

    CA-8743 Fixes build

--- src/background.js
+++ src/background.js
@@ -5,7 +5,6 @@ if (PLATFORM == 'chrome' || PLATFORM == 'firefox') {
     if (PLATFORM == 'chrome') {
       chrome.tabs.executeScript(tabId, {code: 'window.uiPreviewerButtonInjected;'}, function(res) {
         if (res && !res[0]) {
-          // chrome.tabs.executeScript(tabId, {code: 'window.uiPreviewerButtonInjected = true;'});
           chrome.tabs.executeScript(tabId, {file: 'src/ui-previewer-button.js'});
         }
       });
