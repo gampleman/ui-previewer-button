4c65bbb08e3796bc07e8a1763826a635fa0c9785
Jakub Hampl <honitom@seznam.cz>
2016-01-29 15:04:15 +0000

    CA-8743 Ports to other browser vendors

--- src/background.js
+++ src/background.js
@@ -1,18 +1,66 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
-  if (changeInfo.status !== 'complete') { return; }
+if (PLATFORM == 'chrome' || PLATFORM == 'firefox') {
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+    if (changeInfo.status !== 'complete') { return; }
 
-  chrome.tabs.executeScript(tabId, {code: 'window.uiPreviewerButtonInjected;'}, function(res) {
-    if (res && !res[0]) {
-      chrome.tabs.executeScript(tabId, {code: 'window.uiPreviewerButtonInjected = true;'});
+    if (PLATFORM == 'chrome') {
+      chrome.tabs.executeScript(tabId, {code: 'window.uiPreviewerButtonInjected;'}, function(res) {
+        if (res && !res[0]) {
+          // chrome.tabs.executeScript(tabId, {code: 'window.uiPreviewerButtonInjected = true;'});
+          chrome.tabs.executeScript(tabId, {file: 'src/ui-previewer-button.js'});
+        }
+      });
+    } else if(PLATFORM == 'firefox') {
       chrome.tabs.executeScript(tabId, {file: 'src/ui-previewer-button.js'});
     }
   });
-});
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.method === 'getUiPreviewerButtonConfig') {
-    sendResponse(JSON.parse(localStorage.uiPreviewerButtonOptions || '{"repos": {}}'));
-  } else if (request.method === 'setUiPreviewerButtonConfig') {
-    localStorage.uiPreviewerButtonOptions = JSON.stringify(request.data);
+  if (PLATFORM === 'firefox') {
+    chrome.contextMenus.create({
+      type: 'normal',
+      contexts: ['all'],
+      id: 'show-options',
+      title: 'Configure UI Previewer',
+      active: true,
+      selected: true,
+      onclick: function() {
+        chrome.tabs.create({
+          url: '/src/options/index.html'
+        });
+      }
+    });
   }
-});
+}
+
+if (PLATFORM === 'safari') {
+  safari.application.addEventListener('command', function(event) {
+    if (event.command === 'configure') {
+      safari.application.activeBrowserWindow.openTab().url = safari.extension.baseURI + 'src/options/index.html';
+    }
+  }, false);
+}
+
+function getSettings() {
+  return JSON.parse(localStorage.uiPreviewerButtonOptions || '{"repos": {}}');
+}
+
+function setSettings(settings) {
+  localStorage.uiPreviewerButtonOptions = JSON.stringify(settings);
+}
+
+if (PLATFORM == 'chrome' || PLATFORM == 'firefox') {
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method === 'getUiPreviewerButtonConfig') {
+      sendResponse(getSettings());
+    } else if (request.method === 'setUiPreviewerButtonConfig') {
+      setSettings(request.data);
+    }
+  });
+} else if (PLATFORM == 'safari') {
+  safari.application.addEventListener('message', function(event) {
+    if (event.name === 'getUiPreviewerButtonConfig') {
+      event.target.page.dispatchMessage('uiPreviewerResponse', getSettings());
+    } else if (event.name === 'setUiPreviewerButtonConfig') {
+      setSettings(event.message);
+    }
+  }, false);
+}
--- src/ui-previewer-button.js
+++ src/ui-previewer-button.js
@@ -1,135 +1,167 @@
-function main() {
-  chrome.runtime.sendMessage({method: 'getUiPreviewerButtonConfig'}, function(options) {
-    var locationParts = window.location.pathname.split('/'),
-        repoPath = locationParts.slice(1, 3).join('/'),
-        repo = options.repos[repoPath];
+if (PLATFORM === 'chrome' || !window.uiPreviewerButtonInjected) {
+  window.uiPreviewerButtonInjected = true;
 
-    if (!repo) {
-      repo = Object.keys(options.repos).filter(function(name) {
-        var repoRegexp = new RegExp('^' + name.replace('*', '.+') + '$', 'i');
-
-        return repoRegexp.test(repoPath);
-      })[0];
+  function getConfig(callback) {
+    if (PLATFORM === 'chrome' || PLATFORM === 'firefox') {
+      chrome.runtime.sendMessage({method: 'getUiPreviewerButtonConfig'}, callback);
+    } else if (PLATFORM === 'safari') {
+      function handleMessage(event) {
+        if (event.name === 'uiPreviewerResponse') {
+          callback(event.message);
+        }
+        safari.self.removeEventListener("message", handleMessage, false);
+      }
+      safari.self.addEventListener("message", handleMessage, false);
+      safari.self.tab.dispatchMessage('getUiPreviewerButtonConfig');
     }
+  }
 
-    if (repo && window.location.pathname.match(/\/pull\/\d/i)) {
-      attemptButtonInsertion(repo);
-    } else if (locationParts.length === 3) {
-      attemptConfigInsertion(repoPath, options);
+  function setConfig(settings) {
+    if (PLATFORM === 'chrome' || PLATFORM === 'firefox') {
+      chrome.runtime.sendMessage({method: 'setUiPreviewerButtonConfig', data: settings});
+    } else if (PLATFORM === 'safari') {
+      safari.self.tab.dispatchMessage('setUiPreviewerButtonConfig', settings);
+    }
+  }
+
+  function appendIcon(iconName, element) {
+    var span = document.createElement('span');
+    span.className = 'octicon octicon-' + iconName;
+    element.appendChild(span);
+  }
+
+  function main() {
+    getConfig(function(options) {
+      var locationParts = window.location.pathname.split('/'),
+          repoPath = locationParts.slice(1, 3).join('/'),
+          repo = options.repos[repoPath];
+
+      if (!repo) {
+        repo = Object.keys(options.repos).filter(function(name) {
+          var repoRegexp = new RegExp('^' + name.replace('*', '.+') + '$', 'i');
+
+          return repoRegexp.test(repoPath);
+        })[0];
+      }
+
+      if (repo && window.location.pathname.match(/\/pull\/\d/i)) {
+        attemptButtonInsertion(repo);
+      } else if (locationParts.length === 3) {
+        attemptConfigInsertion(repoPath, options);
+      }
+    });
+  }
+
+  function attemptConfigInsertion(repoPath, options) {
+    var currentNode, newConfig,
+        magicHeader = document.querySelector('#user-content-ui-previewer-button-config');
+
+    if (!magicHeader) { return; }
+
+    currentNode = magicHeader.parentNode;
+
+    while (currentNode) {
+      if (currentNode.classList && currentNode.classList.contains('highlight')) {
+        newConfig = JSON.parse(currentNode.textContent);
+        options.repos[repoPath] = newConfig;
+        currentNode = null;
+        setConfig(options);
+      } else {
+        currentNode = currentNode.nextSibling;
+      }
+    }
+  }
+
+  function attemptButtonInsertion(repo) {
+    var link, href;
+    if (repo.branch && !document.querySelector('#partial-discussion-header .uiPreviewer')) {
+      var branch = document.querySelector('.current-branch:last-of-type').innerText.trim();
+      var matches = (new RegExp(repo.branch.pattern, 'gi')).exec(branch);
+      if (matches) {
+        link = document.createElement('a');
+        href = repo.branch.urlPattern.replace(/\$(\d)/, function(a, nstr) {
+          var index = parseInt(nstr, 10);
+          return matches[index];
+        });
+        link.href = href;
+        link.className = 'uiPreviewer';
+        appendIcon(repo.branch.icon, link);
+        link.target = '_blank';
+        document.querySelector('.current-branch:last-of-type').parentElement.appendChild(link);
+      }
+
+    }
+    var parent = document.querySelector('.completeness-indicator-success+* .status-heading');
+    var successfulCommits = Array.prototype.map.call(document.querySelectorAll('.commit-build-statuses  .text-success') || [], function(el) {
+      return el.parentElement.parentElement.querySelector('code>.commit-id');
+    }).filter(function(el) {return el;});
+    if (successfulCommits.length > 0) {
+      if (parent && !parent.querySelector('.uiPreviewer')) {
+        if (repo.mainButton) {
+          var hash = successfulCommits[successfulCommits.length - 1].href.match(/[a-z\d]{40}/i)[0];
+          link = document.createElement('a');
+          href = repo.mainButton.urlPattern.replace(/\${gitSha}/, hash);
+          link.href = href;
+          link.className = 'minibutton uiPreviewer';
+          appendIcon(repo.mainButton.icon, link);
+          link.style.marginRight = '0.2em';
+          link.style.marginLeft = '0.4em';
+          link.target = '_blank';
+          parent.appendChild(link);
+        }
+
+        if (repo.secondary) {
+          var secondary = document.createElement('a');
+          href = repo.secondary.urlPattern.replace(/\${gitSha}/, hash);
+          secondary.href = href;
+          secondary.setAttribute('aria-label', repo.secondary.buttonText);
+          secondary.className = 'minibutton tooltipped tooltipped-s uiPreviewer';
+          appendIcon(repo.secondary.icon, secondary);
+          secondary.style.marginRight = '0.4em';
+          secondary.style.marginLeft = '0.2em';
+          secondary.target = '_blank';
+          parent.appendChild(secondary);
+        }
+        if (document.querySelector('.build-status-description > span:last-of-type')) {
+          document.querySelector('.build-status-description > span:last-of-type').style.width = '60%';
+        }
+      }
+
+      for (var i = 0, l = successfulCommits.length; i < l; i++) {
+        if (!successfulCommits[i].parentElement.previousElementSibling.previousElementSibling) {
+          hash = successfulCommits[i].href.match(/[a-z\d]{40}/i)[0];
+          link = document.createElement('a');
+          link.setAttribute('aria-label', repo.mainButton.buttonText);
+          link.setAttribute('title', repo.mainButton.buttonText);
+          href = repo.mainButton.urlPattern.replace(/\${gitSha}/, hash);
+          link.href = href;
+          link.className = 'uiPreviewer-link';
+          appendIcon(repo.mainButton.icon, link);
+          link.style.color = '#bbb';
+          link.target = '_blank';
+          parent = successfulCommits[i].parentElement.parentElement;
+          parent.insertBefore(link, parent.firstChild);
+        }
+      }
+    }
+  }
+
+  function isGitHubPageTransition(mutation) {
+    return mutation.type === 'childList' && mutation.addedNodes.length;
+  }
+
+  var observer = new MutationObserver(function(mutations) {
+    if (mutations.some(isGitHubPageTransition)) {
+      main();
     }
   });
+
+  observer.observe(document.querySelector('#js-repo-pjax-container'), {
+    childList: true,
+    attributes: false,
+    characterData: false,
+    subtree: true
+  });
+
+  main();
 }
-
-function attemptConfigInsertion(repoPath, options) {
-  var currentNode, newConfig,
-      magicHeader = document.querySelector('#user-content-ui-previewer-button-config');
-
-  if (!magicHeader) { return; }
-
-  currentNode = magicHeader.parentNode;
-
-  while (currentNode) {
-    if (currentNode.classList && currentNode.classList.contains('highlight')) {
-      newConfig = JSON.parse(currentNode.textContent);
-      options.repos[repoPath] = newConfig;
-      currentNode = null;
-
-      chrome.runtime.sendMessage({method: 'setUiPreviewerButtonConfig', data: options});
-    } else {
-      currentNode = currentNode.nextSibling;
-    }
-  }
-}
-
-function attemptButtonInsertion(repo) {
-  var link, href;
-  if (repo.branch && !document.querySelector('#partial-discussion-header .uiPreviewer')) {
-    var branch = document.querySelector('.current-branch:last-of-type').innerText.trim();
-    var matches = (new RegExp(repo.branch.pattern, 'gi')).exec(branch);
-    if (matches) {
-      link = document.createElement('a');
-      href = repo.branch.urlPattern.replace(/\$(\d)/, function(a, nstr) {
-        var index = parseInt(nstr, 10);
-        return matches[index];
-      });
-      link.href = href;
-      link.className = 'uiPreviewer';
-      link.innerHTML = '<span class="octicon octicon-' + repo.branch.icon + '"></span>';
-      link.target = '_blank';
-      document.querySelector('.current-branch:last-of-type').parentElement.appendChild(link);
-    }
-
-  }
-  var parent = document.querySelector('.completeness-indicator-success+* .status-heading');
-  var successfulCommits = Array.prototype.map.call(document.querySelectorAll('.commit-build-statuses  .text-success') || [], function(el) {
-    return el.parentElement.parentElement.querySelector('code>.commit-id');
-  }).filter(function(el) {return el;});
-  if (successfulCommits.length > 0) {
-    if (parent && !parent.querySelector('.uiPreviewer')) {
-      if (repo.mainButton) {
-        var hash = successfulCommits[successfulCommits.length - 1].href.match(/[a-z\d]{40}/i)[0];
-        link = document.createElement('a');
-        href = repo.mainButton.urlPattern.replace(/\${gitSha}/, hash);
-        link.href = href;
-        link.className = 'minibutton uiPreviewer';
-        link.innerHTML = '<span class="octicon octicon-' + repo.mainButton.icon + '"></span>';
-        link.style.marginRight = '0.2em';
-        link.style.marginLeft = '0.4em';
-        link.target = '_blank';
-        parent.appendChild(link);
-      }
-
-      if (repo.secondary) {
-        var secondary = document.createElement('a');
-        href = repo.secondary.urlPattern.replace(/\${gitSha}/, hash);
-        secondary.href = href;
-        secondary.setAttribute('aria-label', repo.secondary.buttonText);
-        secondary.className = 'minibutton tooltipped tooltipped-s uiPreviewer';
-        secondary.innerHTML = '<span class="octicon octicon-' + repo.secondary.icon + '"></span>';
-        secondary.style.marginRight = '0.4em';
-        secondary.style.marginLeft = '0.2em';
-        secondary.target = '_blank';
-        parent.appendChild(secondary);
-      }
-      if (document.querySelector('.build-status-description > span:last-of-type')) {
-        document.querySelector('.build-status-description > span:last-of-type').style.width = '60%';
-      }
-    }
-
-    for (var i = 0, l = successfulCommits.length; i < l; i++) {
-      if (!successfulCommits[i].parentElement.previousElementSibling.previousElementSibling) {
-        hash = successfulCommits[i].href.match(/[a-z\d]{40}/i)[0];
-        link = document.createElement('a');
-        link.setAttribute('aria-label', repo.mainButton.buttonText);
-        link.setAttribute('title', repo.mainButton.buttonText);
-        href = repo.mainButton.urlPattern.replace(/\${gitSha}/, hash);
-        link.href = href;
-        link.className = 'uiPreviewer-link';
-        link.innerHTML = '<span class="octicon octicon-' + repo.mainButton.icon + '"></span> ';
-        link.style.color = '#bbb';
-        link.target = '_blank';
-        parent = successfulCommits[i].parentElement.parentElement;
-        parent.insertBefore(link, parent.firstChild);
-      }
-    }
-  }
-}
-
-function isGitHubPageTransition(mutation) {
-  return mutation.type === 'childList' && mutation.addedNodes.length;
-}
-
-var observer = new MutationObserver(function(mutations) {
-  if (mutations.some(isGitHubPageTransition)) {
-    main();
-  }
-});
-
-observer.observe(document.querySelector('#js-repo-pjax-container'), {
-  childList: true,
-  attributes: false,
-  characterData: false,
-  subtree: true
-});
-
-main();
